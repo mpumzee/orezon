6c17e4d1b7fb19e528cc1ba99693555a971e5316
systems <systems@shearwatervf.com>
2025-01-10 13:27:08 +0200

    corrected the code

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,280 +1,289 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { SearchService } from '../search.service';
-import { Products } from '../../models/products';
-import { ProductsService } from '../../services/products.service';
 import { ProductCategory } from '../../models/product-category';
+import { Products } from '../../models/products';
 import { ProductCategoryService } from '../../services/product-category.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ProductsService } from '../../services/products.service';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent implements OnInit {
+  products: Products[] = [];
 
-  products: Products[] = []
+  sellerId = 6;
 
-  sellerId = 6
+  categories: ProductCategory[] = [];
 
-  categories: ProductCategory[] = []
+  newProduct: Products = {} as Products;
 
-  newProduct: Products = {} as Products
+  selectedProduct: Products = {} as Products;
 
-  selectedProduct: Products = {} as Products
+  selectedCategory: ProductCategory = {} as ProductCategory;
 
-  selectedCategory: ProductCategory = {} as ProductCategory
+  public productForm: FormGroup;
 
-    public productForm: FormGroup;
+  success = false;
 
-    success = false;
+  successMsg: any;
 
-    successMsg: any;
+  errorMsg: any;
 
-    errorMsg: any;
+  error = false;
 
-    error = false
+  editProduct = false;
 
-    editProduct = false
+  selectedFile: File | null = null;
 
-    selectedFile: File | null = null;
+  addProduct_ = false;
 
-    addProduct_ = false
+  title: any;
 
-    title: any
-
-    msg: any
+  msg: any;
 
   equipment: any;
 
-  deleteModal = false
+  deleteModal = false;
 
-  createModal = false
+  createModal = false;
 
-  deletename: any
+  deletename: any;
 
-  viewProduct = false
+  viewProduct = false;
 
   slides: any;
 
-  user: any
+  user: any;
 
-  selectedId=0
+  selectedId = 0;
 
-  constructor(private http: HttpClient, 
-    private router: Router, private searchService: SearchService, private productService: ProductsService, private categoryService: ProductCategoryService){ 
-      this.productForm = new FormGroup({
-        name: new FormControl('', [Validators.required]),     
-        description: new FormControl('', [Validators.required]),     
-        price: new FormControl('', [Validators.required]),
-        quantity: new FormControl('', [Validators.required]),
-        category_id: new FormControl('', [Validators.required]),
-        image_url: new FormControl('')
-      });
-    }
-
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private searchService: SearchService,
+    private productService: ProductsService,
+    private categoryService: ProductCategoryService
+  ) {
+    this.productForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      price: new FormControl('', [Validators.required]),
+      quantity: new FormControl('', [Validators.required]),
+      category_id: new FormControl('', [Validators.required]),
+      image_url: new FormControl(''),
+    });
+  }
 
   ngOnInit() {
-
     this.user = JSON.parse(sessionStorage.getItem('loggedUser') || '{}');
-    console.log(this.user)
+    console.log(this.user);
 
     if (!this.user) {
       this.router.navigate(['/login']);
     }
 
-    this.categoryService.getAllList()
-      .subscribe(res => {
-        this.categories = res.data;
-        console.log('categories:', this.categories)
-      });
+    this.categoryService.getAllList().subscribe((res) => {
+      this.categories = res.data;
+      console.log('categories:', this.categories);
+    });
 
-    this.productService.getSellerProducts(this.user)
-      .subscribe(res => {
-        res.data.forEach((product: any) => {
-          const category = this.categories.filter(x => x.id == product.category_id)
-          category.forEach(cat => {
-            product.category_name = cat.name
-          })
+    this.productService.getSellerProducts(this.user).subscribe((res) => {
+      res.data.forEach((product: any) => {
+        const category = this.categories.filter(
+          (x) => x.id == product.category_id
+        );
+        category.forEach((cat) => {
+          product.category_name = cat.name;
         });
-        this.products = res.data;
-        console.log('products:', this.products)
       });
+      this.products = res.data;
+      console.log('products:', this.products);
+    });
 
-    this.searchService.currentSearchTerm.subscribe(term => {
+    this.searchService.currentSearchTerm.subscribe((term) => {
       if (term) {
         this.searchEquipment(term); // Call with the updated search term
-      }
-      else{
+      } else {
         this.getAllEquipment();
       }
-      
     });
   }
 
-  onChange(event: any){
+  onChange(event: any) {
     const file: File = event.target.files[0];
 
     if (file) {
       this.selectedFile = file;
-      this.productForm.patchValue({image_url: this.selectedFile})
-      console.log(this.productForm)
+      this.productForm.patchValue({ image_url: this.selectedFile });
+      console.log(this.productForm);
     }
   }
 
-
-  addProduct(){
-    console.log(this.productForm.value)
+  addProduct() {
+    console.log(this.productForm.value);
     this.newProduct = this.productForm.value;
-    this.newProduct.user_id = this.user
+    this.newProduct.user_id = this.user;
     const formData = new FormData();
-    formData.append('name', this.productForm.value.name)
-    formData.append('description', this.productForm.value.description)
-    formData.append('price', this.productForm.value.price)
-    formData.append('quantity', this.productForm.value.quantity)
-    formData.append('category_id', this.productForm.value.category_id)
-    formData.append('image_url', this.productForm.value.image_url)
-    formData.append('user_id', this.user)
+    formData.append('name', this.productForm.value.name);
+    formData.append('description', this.productForm.value.description);
+    formData.append('price', this.productForm.value.price);
+    formData.append('quantity', this.productForm.value.quantity);
+    formData.append('category_id', this.productForm.value.category_id);
+    formData.append('image_url', this.productForm.value.image_url);
+    formData.append('user_id', this.user);
 
-    this.createModal = false
+    this.createModal = false;
 
-    this.productService.create(this.productForm.value)
-      .subscribe((res) => {
-        console.log('res',res);
+    this.productService.create(this.productForm.value).subscribe(
+      (res) => {
+        console.log('res', res);
 
         if (res.status == 'created') {
-          this.success = true
-          this.title = res.status
-          this.successMsg = res.message
-          this.productService.getSellerProducts(this.user)
-            .subscribe(res => {
-              res.data.forEach((product: any) => {
-                const category = this.categories.filter(x => x.id == product.category_id)
-                category.forEach(cat => {
-                  product.category_name = cat.name
-                })
+          this.success = true;
+          this.title = res.status;
+          this.successMsg = res.message;
+          this.productService.getSellerProducts(this.user).subscribe((res) => {
+            res.data.forEach((product: any) => {
+              const category = this.categories.filter(
+                (x) => x.id == product.category_id
+              );
+              category.forEach((cat) => {
+                product.category_name = cat.name;
               });
-              this.products = res.data;
-              console.log('products:', this.products)
             });
-          console.log(res.message)
-        }
-        else {
+            this.products = res.data;
+            console.log('products:', this.products);
+          });
           console.log(res.message);
-    // Handle the error as needed
-        }
-
-      },
-        (error) => {
-          console.error(error.error.message);
-          this.error = true
-          this.title = error.error.status
-          this.errorMsg = error.error.message
+        } else {
+          console.log(res.message);
           // Handle the error as needed
-        });
-    this.productForm.reset()
-  }
-
-  updateProduct(){
-    console.log(this.productForm.value, this.selectedId)
-    this.productService.update(this.productForm.value, this.selectedId)
-      .subscribe((res) => {
-        console.log('res',res);
-
-        if (res.status == 'success') {
-          this.success = true
-          this.title = res.status
-          this.successMsg = res.message
-          var index = this.products.findIndex(x => x.id === this.selectedId);
-          this.products.splice(index, 1);
-
-          this.products = [...this.products, res.data];
         }
-        else {
-          console.error(Error);
-        }
-
       },
-        (error) => {
-          console.error(error.message);
-          this.error = true
-          this.title = error.error.status
-          this.errorMsg = error.error.message
-        });
-        
+      (error) => {
+        console.error(error.error.message);
+        this.error = true;
+        this.title = error.error.status;
+        this.errorMsg = error.error.message;
+        // Handle the error as needed
+      }
+    );
     this.productForm.reset();
-    this.editProduct = false
   }
 
-  clear(){
-    this.productForm.reset()
-  }
+  updateProduct() {
+    console.log(this.productForm.value, this.selectedId);
+    this.productService
+      .update(this.productForm.value, this.selectedId)
+      .subscribe(
+        (res) => {
+          console.log('res', res);
 
-  hideDialog(){
-    this.success = false
-    this.error = false
-    this.deleteModal = false
-    this.editProduct = false
-    this.viewProduct = false
-  }
+          if (res.status == 'success') {
+            this.success = true;
+            this.title = res.status;
+            this.successMsg = res.message;
+            var index = this.products.findIndex(
+              (x) => x.id === this.selectedId
+            );
+            this.products.splice(index, 1);
 
-  edit(item: any){
-    this.editProduct = true
-    this.selectedProduct = item
-    this.selectedId = item.id
-    console.log(this.selectedProduct)
-    this.productForm = new FormGroup({
-      name: new FormControl(this.selectedProduct.name, [Validators.required]),     
-      description: new FormControl(this.selectedProduct.description, [Validators.required]),     
-      price: new FormControl(this.selectedProduct.price, [Validators.required]),
-      quantity: new FormControl(this.selectedProduct.quantity, [Validators.required]),
-      category_id: new FormControl(this.selectedProduct.category_id, [Validators.required]),
-    });
-    console.log(this.productForm.value)
-  }
-
-  view(item: any){
-    this.selectedProduct = item
-    this.viewProduct = true
-  }
-
-  delete(item: any){
-    this.deletename = item.name
-    this.selectedId = item.id
-    this.deleteModal = true
-  }
-
-  showModal(){
-    this.createModal = true
-  }
-
-  confirmDelete(){
-    this.productService.delete(this.selectedId)
-      .subscribe((res) => {
-        console.log('res',res);
-
-          this.success = true
-          this.title = 'SUCESS'
-          this.successMsg = this.deletename + ' deleted successfully'
-          var index = this.products.findIndex(x => x.id === this.selectedId);
-          this.products.splice(index, 1);
-
-          this.products = [...this.products, res.data];
-
-      },
+            this.products = [...this.products, res.data];
+          } else {
+            console.error(Error);
+          }
+        },
         (error) => {
           console.error(error.message);
-          this.error = true
-          this.title = error.error.status
-          this.errorMsg = error.error.message
-        });
+          this.error = true;
+          this.title = error.error.status;
+          this.errorMsg = error.error.message;
+        }
+      );
 
-    this.deleteModal = false
+    this.productForm.reset();
+    this.editProduct = false;
   }
 
-  getAllEquipment(){
+  clear() {
+    this.productForm.reset();
+  }
+
+  hideDialog() {
+    this.success = false;
+    this.error = false;
+    this.deleteModal = false;
+    this.editProduct = false;
+    this.viewProduct = false;
+    this.createModal = false;
+  }
+
+  edit(item: any) {
+    this.editProduct = true;
+    this.selectedProduct = item;
+    this.selectedId = item.id;
+    console.log(this.selectedProduct);
+    this.productForm = new FormGroup({
+      name: new FormControl(this.selectedProduct.name, [Validators.required]),
+      description: new FormControl(this.selectedProduct.description, [
+        Validators.required,
+      ]),
+      price: new FormControl(this.selectedProduct.price, [Validators.required]),
+      quantity: new FormControl(this.selectedProduct.quantity, [
+        Validators.required,
+      ]),
+      category_id: new FormControl(this.selectedProduct.category_id, [
+        Validators.required,
+      ]),
+    });
+    console.log(this.productForm.value);
+  }
+
+  view(item: any) {
+    this.selectedProduct = item;
+    this.viewProduct = true;
+  }
+
+  delete(item: any) {
+    this.deletename = item.name;
+    this.selectedId = item.id;
+    this.deleteModal = true;
+  }
+
+  showModal() {
+    this.createModal = true;
+  }
+
+  confirmDelete() {
+    this.productService.delete(this.selectedId).subscribe(
+      (res) => {
+        console.log('res', res);
+
+        this.success = true;
+        this.title = 'SUCESS';
+        this.successMsg = this.deletename + ' deleted successfully';
+        var index = this.products.findIndex((x) => x.id === this.selectedId);
+        this.products.splice(index, 1);
+
+        this.products = [...this.products, res.data];
+      },
+      (error) => {
+        console.error(error.message);
+        this.error = true;
+        this.title = error.error.status;
+        this.errorMsg = error.error.message;
+      }
+    );
+
+    this.deleteModal = false;
+  }
+
+  getAllEquipment() {
     const url = `http://localhost:8000/api/v1/equipment`;
     this.http.get(url).subscribe({
       next: (data: any) => {
@@ -284,34 +293,34 @@ export class ProductsComponent implements OnInit {
       error: (error: any) => {
         console.error(error);
         // Handle the error as needed
-      }
+      },
     });
-    }
+  }
 
-  searchEquipment(name?: string){
+  searchEquipment(name?: string) {
     const url = `http://localhost:8000/api/v1/equipment/search/${name}`;
     this.http.get(url).subscribe({
       next: (data: any) => {
-        console.log("Search result ",data);
+        console.log('Search result ', data);
         this.slides = data;
       },
       error: (error: any) => {
         console.error(error);
         // Handle the error as needed
-      }
+      },
     });
-    }
+  }
 
-    // slides = [
-    //   { img: 'http://placehold.it/350x150/000000' },
-    //   { img: 'http://placehold.it/350x150/111111' },
-    //   // more slides...
-    // ];
-    
-    slideConfig = {
-      slidesToShow: 4, // Adjust based on how many slides you want visible
-      slidesToScroll: 1,
-      dots: true,
-      infinite: true,
+  // slides = [
+  //   { img: 'http://placehold.it/350x150/000000' },
+  //   { img: 'http://placehold.it/350x150/111111' },
+  //   // more slides...
+  // ];
+
+  slideConfig = {
+    slidesToShow: 4, // Adjust based on how many slides you want visible
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
   };
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,28 +1,27 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { PackagesService } from '../../services/packages.service';
-import { Country } from '../../enums/country';
 import { Router } from '@angular/router';
-import { User } from '../../models/user';
-import { Package } from '../../models/package';
-import { SellerRegistrationService } from '../../services/seller-registration.service';
-import { Seller } from '../../models/seller';
+import { Country } from '../../enums/country';
 import { BankDetails } from '../../models/bank-details';
+import { Package } from '../../models/package';
+import { Seller } from '../../models/seller';
+import { User } from '../../models/user';
+import { PackagesService } from '../../services/packages.service';
+import { SellerRegistrationService } from '../../services/seller-registration.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
-
   countries: any[] = [];
+
+  editProfileModal = false;
 
   public sellerForm: FormGroup;
 
-  public bankForm: FormGroup;
-
-  user: User = {} as User
+  user: User = {} as User;
 
   success = false;
 
@@ -30,128 +29,174 @@ export class ProfileComponent {
 
   errorMsg: any;
 
-  error = false
+  error = false;
 
-  title: any
+  title: any;
 
-  msg: any
+  msg: any;
 
-  packages: Package[] = []
+  packages: Package[] = [];
 
-  curentSellerDetails: Seller = {} as Seller
+  curentSellerDetails: Seller = {} as Seller;
 
-  bankDetails: BankDetails = {} as BankDetails
+  bankDetails: BankDetails = {} as BankDetails;
 
-  role: any
+  role: any;
 
-  bank_details: BankDetails = {} as BankDetails
+  bank_details: BankDetails = {} as BankDetails;
 
-  selectedPackage: Package = {} as Package
+  selectedPackage: Package = {} as Package;
 
-  packageId = 0
+  packageId = 0;
 
-  constructor(private packageService: PackagesService, private router: Router, private sellerService: SellerRegistrationService){
-      this.sellerForm = new FormGroup({
-        business_name: new FormControl('', [Validators.required]),     
-        country: new FormControl('', [Validators.required]),     
-        id_number: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
-        phone: new FormControl('', [Validators.required, Validators.minLength(10)]),
-        // name: new FormControl('', [Validators.required]),     
-        // email: new FormControl('', [Validators.email]), 
-        // password: new FormControl('', [Validators.required,Validators.minLength(8)]),
-        // password_confirmation: new FormControl('', [Validators.required]),
-      });
-      this.bankForm = new FormGroup({
-        bank: new FormControl('', [Validators.required]),     
-        account_number: new FormControl('', [Validators.required]),     
-        branch: new FormControl('', [Validators.required]),
-        branch_code: new FormControl('', [Validators.required]),
-      });
+  constructor(
+    private packageService: PackagesService,
+    private router: Router,
+    private sellerService: SellerRegistrationService
+  ) {
+    this.sellerForm = new FormGroup({
+      business_name: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
+      id_number: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      bank: new FormControl('', [Validators.required]),
+      account_number: new FormControl('', [Validators.required]),
+      branch: new FormControl('', [Validators.required]),
+      branch_code: new FormControl('', [Validators.required]),
+      // name: new FormControl('', [Validators.required]),
+      // email: new FormControl('', [Validators.email]),
+      // password: new FormControl('', [Validators.required,Validators.minLength(8)]),
+      // password_confirmation: new FormControl('', [Validators.required]),
+    });
+  }
+
+  ngOnInit(): void {
+    this.user.id = JSON.parse(sessionStorage.getItem('loggedUser') || '{}');
+    this.role = sessionStorage.getItem('loggedUserRole') || '{}';
+    this.user.name = sessionStorage.getItem('loggedUserName') || '{}';
+    this.user.email = sessionStorage.getItem('loggedUserEmail') || '{}';
+    this.bankDetails.account_number =
+      sessionStorage.getItem('loggedUserAccountNumber') || '{}';
+    this.bankDetails.bank = sessionStorage.getItem('loggedUserBank') || '{}';
+    this.bankDetails.branch =
+      sessionStorage.getItem('loggedUserBranch') || '{}';
+    this.bankDetails.branch_code =
+      sessionStorage.getItem('loggedUserBranchCode') || '{}';
+    this.bankDetails.id = JSON.parse(
+      sessionStorage.getItem('loggedUserBankDetailsId') || '{}'
+    );
+    console.log('details', this.bankDetails);
+
+    if (!this.user) {
+      this.router.navigate(['/login']);
     }
 
-    ngOnInit(): void{
+    this.sellerService.get(this.user.id).subscribe((res) => {
+      this.curentSellerDetails = res.data;
+      console.log('seller:', res.data);
+    });
 
-      this.user.id = JSON.parse(sessionStorage.getItem('loggedUser') || '{}');
-      this.role = (sessionStorage.getItem('loggedUserRole') || '{}');
-      this.user.name = (sessionStorage.getItem('loggedUserName') || '{}');
-      this.user.email = (sessionStorage.getItem('loggedUserEmail') || '{}');
-      this.bankDetails.account_number = (sessionStorage.getItem('loggedUserAccountNumber') || '{}');
-      this.bankDetails.bank = (sessionStorage.getItem('loggedUserBank') || '{}');
-      this.bankDetails.branch = (sessionStorage.getItem('loggedUserBranch') || '{}');
-      this.bankDetails.branch_code = (sessionStorage.getItem('loggedUserBranchCode') || '{}');
-      this.bankDetails.id = JSON.parse(sessionStorage.getItem('loggedUserBankDetailsId') || '{}');
-      console.log('details',this.bankDetails)
-
-      if (!this.user) {
-        this.router.navigate(['/login']);
+    this.packageService.getAllList().subscribe((res) => {
+      this.packages = res.data;
+      if (!this.packages) {
+        console.log('no package');
+      } else {
+        this.packageId! = JSON.parse(
+          sessionStorage.getItem('loggedUserPackageId') || '{}'
+        )!;
+        this.packages
+          .filter((x) => x.id == this.packageId)
+          .forEach((pack) => {
+            this.selectedPackage = pack;
+          });
       }
 
-      this.sellerService.get(this.user.id)
-            .subscribe(res => {
-              this.curentSellerDetails = res.data;
-              console.log('seller:', res.data)
-            });
+      console.log('packages:', res.data);
+    });
 
-      this.packageService.getAllList()
-            .subscribe(res => {
-              this.packages = res.data;
-              if(!this.packages){
-                console.log('no package')
-              } else{
-              this.packageId! = JSON.parse(sessionStorage.getItem('loggedUserPackageId') || '{}')!;
-                this.packages.filter(x => x.id == this.packageId).forEach(pack =>{
-                  this.selectedPackage = pack
-                })
-              }
-            
-              console.log('packages:', res.data)
-            });
+    this.countries = Object.values(Country);
+  }
 
-        this.countries = Object.values(Country)
-      }
+  edit() {
+    this.sellerForm = new FormGroup({
+      business_name: new FormControl(this.curentSellerDetails.business_name, [
+        Validators.required,
+      ]),
+      country: new FormControl(this.curentSellerDetails.country, [
+        Validators.required,
+      ]),
+      id_number: new FormControl(this.curentSellerDetails.id_number, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ]),
+      phone: new FormControl(this.curentSellerDetails.phone, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      bank: new FormControl(this.bankDetails.bank, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      account_number: new FormControl(this.bankDetails.account_number, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      branch: new FormControl(this.bankDetails.branch, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      branch_code: new FormControl(this.bankDetails.branch_code, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+    });
 
-      edit(){
-        this.sellerForm = new FormGroup({
-          business_name: new FormControl(this.curentSellerDetails.business_name, [Validators.required]),     
-          country: new FormControl(this.curentSellerDetails.country, [Validators.required]),     
-          id_number: new FormControl(this.curentSellerDetails.id_number, [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
-          phone: new FormControl(this.curentSellerDetails.phone, [Validators.required, Validators.minLength(10)]),
-        });
-      }
+    console.log('yes');
 
-    update(){
-      this.sellerService.update(this.sellerForm.value, this.curentSellerDetails.id)
-              .subscribe((res) => {
-        
-                if (res.status == 'success') {
-                  console.log(res.message)
-                  this.success = true
-                  this.title = res.status;
-                  this.successMsg = res.message
-                  this.ngOnInit()
-                }
-                else {
-                  console.error(Error);
-                }
-        
-              },
-                (error) => {
-                  console.error(error.error.message);
-                  this.error = true
-                  this.title = error.error.status
-                  this.errorMsg = error.error.message
-                });
-                
-            this.sellerForm.reset();
-    }
+    this.editProfileModal = true;
+  }
 
-    changePackage(){
-      this.router.navigate(['/select-package']);
-    }
+  update() {
+    this.sellerService
+      .update(this.sellerForm.value, this.curentSellerDetails.id)
+      .subscribe(
+        (res) => {
+          if (res.status == 'success') {
+            console.log(res.message);
+            this.success = true;
+            this.title = res.status;
+            this.successMsg = res.message;
+            this.ngOnInit();
+          } else {
+            console.error(Error);
+          }
+        },
+        (error) => {
+          console.error(error.error.message);
+          this.error = true;
+          this.title = error.error.status;
+          this.errorMsg = error.error.message;
+        }
+      );
 
-    hideDialog(){
-      this.success = false
-      this.error = false
-    }
+    this.sellerForm.reset();
+  }
 
+  changePackage() {
+    this.router.navigate(['/select-package']);
+  }
+
+  hideDialog() {
+    this.success = false;
+    this.error = false;
+    this.editProfileModal = false;
+  }
 }
