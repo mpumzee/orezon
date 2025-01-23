24a4de80efe1114393511213b98a7b19170695d5
Tello <pateltanaka22@live.com>
2025-01-23 07:52:08 +0200

    wish list

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,4 @@ export * from './orders.service';
 export * from './paypal.service';
 export * from './product-category.service';
 export * from  './seller-cart.service';
+export * from  './wish-list.service';
