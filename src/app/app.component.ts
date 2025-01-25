54c540121e82123b78c4e31c6e6d9f04586dcfc1
Tello <pateltanaka22@live.com>
2025-01-25 18:37:29 +0200

    remvoed loader servivses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { LoaderService, AlertService } from './tools/services';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,25 @@ import { initFlowbite } from 'flowbite';
 })
 export class AppComponent {
   title = 'orezon';
+  load = false
+
+  constructor(    public loaderService: LoaderService,
+    public alertService:AlertService,){
+      this.loaderService.isLoading.subscribe((resp) => {
+        this.load = resp;
+      });
+    }
 
   ngOnInit(): void {
     initFlowbite();
+    this.loaderService.isLoading.subscribe(resp =>{
+      this.load = resp;
+      if(this.load){
+        this.alertService.loader();
+      }else{
+        this.alertService.closeLoader();
+      }
+
+    });
   }
 }
