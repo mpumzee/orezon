b9ca0dd228f71de803ec6e2870760d054575ccbb
systems <systems@shearwatervf.com>
2025-01-27 00:56:19 +0200

    updated quotation

--- src/app/pages/quotation/quotation.component.ts
+++ src/app/pages/quotation/quotation.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import jsPDF from 'jspdf';
+import { User } from '../../../models/user';
 import { WishListService } from '../../tools/services';
 
 @Component({
@@ -15,9 +17,21 @@ export class QuotationComponent {
 
   todayDate = new Date()
 
-  constructor(private wishlistService: WishListService) { }
+  role: any
+
+  user: User = {} as User;
+
+  constructor(private router: Router, private wishlistService: WishListService) { }
 
   ngOnInit(): void {
+    this.role = sessionStorage.getItem('loggedUserRole') || '{}';
+    this.user.name = sessionStorage.getItem('loggedUserName') || '{}';
+    this.user.email = sessionStorage.getItem('loggedUserEmail') || '{}';
+
+    if (sessionStorage.length == 0) {
+      this.router.navigate(['/login']);
+    }
+
     this.wishlist = this.wishlistService.getCurrentCart()
     this.wishlistCount = this.wishlistService.getTotal()
     this.wishlist.forEach(wish => {
