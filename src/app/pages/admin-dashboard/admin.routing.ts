db00f86d34c148df418f668ba41e0ef2efe5be70
Tello <pateltanaka22@live.com>
2025-01-30 20:56:45 +0200

    routing

--- src/app/pages/admin-dashboard/admin.routing.ts
+++ src/app/pages/admin-dashboard/admin.routing.ts
@@ -1,53 +1,52 @@
 import { RouterModule, Routes } from '@angular/router';
-import { AdminDashboardComponent } from './admin-dashboard.component';
-import { UsersComponent } from '../users/users.component';
-import { OrdersComponent } from '../orders/orders.component';
-import { CategoriesComponent } from '../categories/categories.component';
-import { PackagesService, PaymentService } from '../../tools/services';
-import { SellerPayoutsComponent } from '../seller-payouts/seller-payouts.component';
-import { AdminStatsComponent } from '../admin-stats/admin-stats.component';
-import { TransactionComponent } from '../transaction/transaction.component';
-import { PackagesComponent } from '../packages/packages.component';
 import { AdminPaymentComponent } from '../admin-payment/admin-payment.component';
+import { AdminStatsComponent } from '../admin-stats/admin-stats.component';
+import { CategoriesComponent } from '../categories/categories.component';
+import { OrdersComponent } from '../orders/orders.component';
+import { PackagesComponent } from '../packages/packages.component';
+import { SellerPayoutsComponent } from '../seller-payouts/seller-payouts.component';
+import { TransactionComponent } from '../transaction/transaction.component';
+import { UsersComponent } from '../users/users.component';
+import { AdminDashboardComponent } from './admin-dashboard.component';
 
 
 const routes: Routes = [
     {
         path: '',
         component: AdminDashboardComponent,
-        children:[
+        children: [
             {
-                path:'',
-                component:AdminStatsComponent
+                path: '',
+                component: AdminStatsComponent
 
             },
             {
-                path:'users',
-                component:UsersComponent
+                path: 'users',
+                component: UsersComponent
             },
             {
-                path:'orders',
-                component:OrdersComponent
+                path: 'orders',
+                component: OrdersComponent
             },
             {
-                path:'categories',
-                component:CategoriesComponent
+                path: 'categories',
+                component: CategoriesComponent
             },
             {
-                path:'payments',
-                component:AdminPaymentComponent
+                path: 'payments',
+                component: AdminPaymentComponent
             },
             {
-                path:'packages',
-                component:PackagesComponent
+                path: 'packages',
+                component: PackagesComponent
             },
             {
-                path:'payouts',
-                component:SellerPayoutsComponent
+                path: 'payouts',
+                component: SellerPayoutsComponent
             },
             {
-                path:'transactions',
-                component:TransactionComponent
+                path: 'transactions',
+                component: TransactionComponent
             },
         ]
     },
