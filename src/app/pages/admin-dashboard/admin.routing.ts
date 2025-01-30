f9416f062a850518f5ff95c9965b85ec6a0d88d6
Tello <pateltanaka22@live.com>
2025-01-30 20:41:04 +0200

    admin routing

--- src/app/pages/admin-dashboard/admin.routing.ts
+++ src/app/pages/admin-dashboard/admin.routing.ts
@@ -1,6 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard.component';
 import { UsersComponent } from '../users/users.component';
+import { OrdersComponent } from '../orders/orders.component';
+import { CategoriesComponent } from '../categories/categories.component';
+import { PackagesService, PaymentService } from '../../tools/services';
+import { SellerPayoutsComponent } from '../seller-payouts/seller-payouts.component';
 
 
 const routes: Routes = [
@@ -11,10 +15,34 @@ const routes: Routes = [
             {
                 path:'users',
                 component:UsersComponent
-            }
+            },
+            {
+                path:'orders',
+                component:OrdersComponent
+            },
+            {
+                path:'categories',
+                component:CategoriesComponent
+            },
+            {
+                path:'payments',
+                component:PaymentService
+            },
+            {
+                path:'packages',
+                component:PackagesService
+            },
+            {
+                path:'payouts',
+                component:SellerPayoutsComponent
+            },
+            {
+                path:'transactions',
+                component:UsersComponent
+            },
         ]
     },
-    
+
 ];
 
 export const AdminRoutes = RouterModule.forChild(routes);
