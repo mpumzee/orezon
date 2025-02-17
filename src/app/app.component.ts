b85dac7b099d6a39e89f7539740f614e7631a597
Tello <pateltanaka22@live.com>
2025-02-17 21:01:06 +0200

    meta data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { LoaderService, AlertService } from './tools/services';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent {
   title = 'orezon';
   load = false
 
-  constructor(    public loaderService: LoaderService,
+  constructor(    public loaderService: LoaderService,private meta: Meta,
     public alertService:AlertService,){
       this.loaderService.isLoading.subscribe((resp) => {
         this.load = resp;
@@ -29,5 +30,9 @@ export class AppComponent {
       }
 
     });
+    this.meta.addTags([
+      { name: 'description', content: 'Join our community today and experience the future of minerals and mining equipment trading   Ore-zone is a pioneering online marketplace that facilitates the buying and selling of minerals and mining equipment. Registered with the Government of South Africa, our platform provides a secure,transparent, and efficient way for businesses and individuals to connect and trade.' },
+      { name: 'keywords', content: 'orezon ,mining ,minerals , equpipement ,' }
+  ]);
   }
 }
