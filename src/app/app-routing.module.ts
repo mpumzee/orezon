393397c72eaa3c7fd782ab2289815be809ed5a10
Tello <pateltanaka22@live.com>
2025-02-17 10:50:32 +0200

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { CategoryShopComponent } from './pages/category-shop/category-shop.compo
 import { CheckOutComponent } from './pages/check-out/check-out.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { EmailVerifiedComponent } from './pages/email-verified/email-verified.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MetalsComponent } from './pages/metals/metals.component';
 import { PackagesComponent } from './pages/packages/packages.component';
@@ -30,6 +31,7 @@ const routes: Routes = [
   { path: '', component: TopNavComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   {
     path: 'admin-dashboard',
     loadChildren: () => import('./pages/admin-dashboard/admin.module').then(m => m.AdminModule)
---
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgot-password.component.html',
+  styleUrl: './forgot-password.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ForgotPasswordComponent { }
