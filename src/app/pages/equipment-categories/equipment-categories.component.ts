70537f18409077f97494f2704250f30a07775bc6
systems <systems@shearwatervf.com>
2025-02-17 16:40:23 +0200

    added categories images

--- src/app/pages/equipment-categories/equipment-categories.component.ts
+++ src/app/pages/equipment-categories/equipment-categories.component.ts
@@ -55,24 +55,88 @@ export class EquipmentCategoriesComponent {
       });
       this.drillingSubCategories.forEach((product: SubCategory) => {
         product.img_url = 'assets/img/drill_rig.png';
+        if (product.id == 9) {
+          product.img_url = 'assets/img/jack_hammers.jpeg'
+        }
+        if (product.id == 8) {
+          product.img_url = 'assets/img/pneumatic_drills.jpeg'
+        }
+        if (product.id == 7) {
+          product.img_url = 'assets/img/rotary_drills.jpeg'
+        }
       });
       this.haulingSubCategories.forEach((product: SubCategory) => {
         product.img_url = 'assets/img/hauling.png';
+        if (product.id == 10) {
+          product.img_url = 'assets/img/loaders.jpeg'
+        }
+        if (product.id == 11) {
+          product.img_url = 'assets/img/load_haul.jpeg'
+        }
+        if (product.id == 12) {
+          product.img_url = 'assets/img/conveyor_ belt.jpeg'
+        }
       });
       this.crushingSubCategories.forEach((product: SubCategory) => {
         product.img_url = 'assets/img/crushing.png';
+        if (product.id == 16) {
+          product.img_url = 'assets/img/ball_mill.jpeg'
+        }
+        if (product.id == 15) {
+          product.img_url = 'assets/img/conec_crushers.jpeg'
+        }
+        if (product.id == 33) {
+          product.img_url = 'assets/img/jaw_crushers.jpeg'
+        }
+        if (product.id == 17) {
+          product.img_url = 'assets/img/roda_mills.jpeg'
+        }
+
       });
       this.separationSubCategories.forEach((product: SubCategory) => {
         product.img_url = 'assets/img/separation.png';
+        if (product.id == 20) {
+          product.img_url = 'assets/img/centrifuges.jpeg'
+        }
+        if (product.id == 19) {
+          product.img_url = 'assets/img/magnetic_separators.jpeg'
+        }
+        if (product.id == 21) {
+          product.img_url = 'assets/img/spiral_concentrators.jpeg'
+        }
       });
       this.undergroundSubCategories.forEach((product: SubCategory) => {
         product.img_url = 'assets/img/underground.png';
+        if (product.id == 24) {
+          product.img_url = 'assets/img/scissor_lifts.jpeg'
+        }
+        if (product.id == 25) {
+          product.img_url = 'assets/img/underground_trucks.jpeg'
+        }
       });
       this.safetySubCategories.forEach((product: SubCategory) => {
         product.img_url = 'assets/img/safety.png';
+        if (product.id == 29) {
+          product.img_url = 'assets/img/fall_protection.jpeg'
+        }
+        if (product.id == 28) {
+          product.img_url = 'assets/img/respirators.jpeg'
+        }
       });
       this.miscellaneousSubCategories.forEach((product: SubCategory) => {
         product.img_url = 'assets/img/miscellaneous.png';
+        if (product.id == 32) {
+          product.img_url = 'assets/img/conveyor_ belt.jpeg'
+        }
+        if (product.id == 33) {
+          product.img_url = 'assets/img/explosives.jpeg'
+        }
+        if (product.id == 30) {
+          product.img_url = 'assets/img/generators.jpeg'
+        }
+        if (product.id == 31) {
+          product.img_url = 'assets/img/pumps.jpeg'
+        }
       });
       this.subCategories = res.data;
       console.log('subCategories:', this.subCategories);
