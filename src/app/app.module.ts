b62a410b016f03762fef7b3fe6d467b4f7ad10e8
Tello <pateltanaka22@live.com>
2025-03-13 22:38:07 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NgxPayPalModule } from 'ngx-paypal';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
